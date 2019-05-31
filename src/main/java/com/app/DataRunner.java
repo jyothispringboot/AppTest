e17f0bd7662bcf1cf9b77ac4bc15b44baeed2a29
Jyothi <Jyothi@JyothiPC>
2019-05-31 11:24:14 +0530

    Learning how to use GitHub

---
+++ src/main/java/com/app/DataRunner.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class DataRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+			System.out.println("Hello....");
+		
+	}
+
+}
